4ece686195059d9dd558a7e0810a00002352b1fa
Hüseyin Demir <demirhuseyinn.94@gmail.om>
2017-08-19 11:55:08 +0300

    Login and Authorization

---
+++ SQLScripts/SQLScripts/LoginandAuthoration.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE Worker (
+    WorkerID int NOT NULL IDENTITY(1,1),
+    LastName varchar(255),
+    FirstName varchar(255),
+    EMail varchar(255),
+	Phone varchar(255),
+	PRIMARY KEY(WorkerID)
+);
+
+CREATE TABLE AuthorizationValue (
+    AuthorizationID int NOT NULL IDENTITY(1,1),
+    AuthorizationValue varchar(255),
+	PRIMARY KEY(AuthorizationID)
+);
+
+CREATE TABLE UserAuthorization (
+    WorkerID int,
+    AuthorizationID int,
+   PRIMARY KEY(WorkerID,AuthorizationID),
+	FOREIGN KEY (WorkerID) REFERENCES Worker(WorkerID),
+	FOREIGN KEY (AuthorizationID) REFERENCES AuthorizationValue(AuthorizationID),
+
+);
+
